190e9f0a9faa1a3926c54c17c74674a6c9abc764
coreoceantesting <coreoceantesting@gmail.com>
2024-01-25 15:02:55 +0530

    add logo in vardi ahaval pdf

--- resources/views/generateslips/vardi_ahaval_pdf.blade.php
+++ resources/views/generateslips/vardi_ahaval_pdf.blade.php
@@ -100,6 +100,7 @@
 </head>
 <body>
     <div class="container">
+        <p><img class="logo" src="{{ public_path('/admin/images/Panvel_Municipal_Corporation.png') }}" height="80" width="80" alt="Left Logo"></p>
         <h5>परिशिष्ट - " ग "</h5>
         <p>( ३ व ३ (ब) )</p>
         <p><b>पनवेल महानगरपालिका अग्निशमन सेवा</b></p>
