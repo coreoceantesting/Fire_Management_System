d450436d87538f86bb4e4ce6563d2a273de58894
coreoceantesting <coreoceantesting@gmail.com>
2024-07-15 15:37:18 +0530

    notification only show cfo

--- resources/views/components/admin/header.blade.php
+++ resources/views/components/admin/header.blade.php
@@ -158,11 +158,12 @@
                     </div>
                 </div>
 
-
-                <div id="notification-bell" style="cursor: pointer;">
-                    <i class="fa fa-bell" style="font-size: 25px;color:gold"></i>
-                    <span id="notification-count" style="color:white;font-weight:bold"></span>
-                </div>
+                @if ( auth()->user()->name === 'CFO' )
+                    <div id="notification-bell" style="cursor: pointer;">
+                        <i class="fa fa-bell" style="font-size: 25px;color:gold"></i>
+                        <span id="notification-count" style="color:white;font-weight:bold"></span>
+                    </div>
+                @endif
 
                 <!-- Modal -->
                 <div class="modal fade" id="notificationModal" tabindex="-1" role="dialog" aria-labelledby="notificationModalLabel" aria-hidden="true">
